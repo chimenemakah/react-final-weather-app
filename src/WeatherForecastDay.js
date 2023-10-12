54fa15b0484292cc6ccd619c0f4ec291cfb7d8b0
chimenemakah <chimenemakah58@gmail.com>
2023-10-12 12:42:50 +0100

    added a file Weatherforecast.js

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+// import WeatherIcon from "./WeatherIcon";
+import "./WeatherIcon.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay.js";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (loaded) {
+    // console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
